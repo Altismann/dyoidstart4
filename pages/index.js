45187f9f0a908f9faca7665784abc192c8317948
Altismann <87611738+Altismann@users.noreply.github.com>
2024-10-17 21:53:12 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -49,7 +49,7 @@ const Home = (props) => {
           <div className="home-container2">
             <div className="home-buttons">
               <a
-                href="https://shop.dyoid.se/collections/all"
+                href="https://dyoid.se"
                 target="_blank"
                 rel="noreferrer noopener"
                 className="home-link3"
